229d809802db9a2892cf3251b2801f741513d301
vadim_budarin <budarin-vv@yandex.ru>
2018-09-14 03:51:33 +0300

    fix typo

--- src/server/utils/getPreLoadingLink.ts
+++ src/server/utils/getPreLoadingLink.ts
@@ -7,7 +7,7 @@ function getPreLoadingLink(resourcePath: string): string {
     const ext = resourcePath.split('.').pop();
     const resourceType = ext && resourceTypes[ext];
 
-    return `<link rel="preload" href="${resourcePath}" as="${resourceType}" >;`;
+    return `<link rel="preload" href="${resourcePath}" as="${resourceType}" >`;
 }
 
 export default getPreLoadingLink;
