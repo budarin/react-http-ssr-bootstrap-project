e8888f866527e2e0716e858a72d9536276cbe3f0
vadim_budarin <budarin-vv@yandex.ru>
2018-09-27 21:36:10 +0300

    refactoring

--- src/utils/getLogger.ts
+++ src/utils/getLogger.ts
@@ -1,6 +1,7 @@
 import pino from 'pino';
 
+const pinoOptions = __DEV__ ? { prettyPrint: { colorize: true } } : {};
 // @ts-ignore
-const logger = pino({ prettyPrint: { colorize: true } });
+const logger = pino(pinoOptions);
 
 export default logger;
