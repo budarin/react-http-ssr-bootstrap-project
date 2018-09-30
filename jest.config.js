6a49184e742a156395710a5347e4cd551bf64535
vadim_budarin <budarin-vv@yandex.ru>
2018-09-30 17:42:03 +0300

    test: update config & snap

--- jest.config.js
+++ jest.config.js
@@ -16,7 +16,7 @@ module.exports = {
     moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
     moduleNameMapper: {
         '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-            '../../config/jest/mocks/fileMock.js',
+            '../../../config/jest/mocks/fileMock.js',
         '\\.(css)$': 'identity-obj-proxy',
     },
     testPathIgnorePatterns: ['/node_modules/'],
