b8eb86a874d5a0be11df3b66345cca3402adacb9
vadim_budarin <budarin-vv@yandex.ru>
2018-10-13 00:29:32 +0300

    add post-merge hook

--- husky.config.js
+++ husky.config.js
@@ -3,5 +3,6 @@ module.exports = {
         'pre-commit': 'lint-staged',
         'pre-push': 'npm test',
         'post-checkout': 'check-dependencies --install',
+        'post-merge': 'check-dependencies --install',
     },
 };
