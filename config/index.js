efac0115ae146bc665ca295bc33f45ebb61180af
vadim_budarin <budarin-vv@yandex.ru>
2018-11-18 20:05:59 +0300

    refactor

--- config/index.js
+++ config/index.js
@@ -1,10 +1,12 @@
+const DEV = process.env.NODE_ENV === 'development';
+
 // if (__DEV__)
 {
     // tslint:disable-next-line
     const dotenv = require('dotenv');
     const result = dotenv.config();
 
-    if (__DEV__) {
+    if (DEV) {
         if (result.error) {
             throw result.error;
         }
@@ -23,37 +25,16 @@ const {
     KEYS_FOLDER,
 } = process.env;
 
-interface IEnv {
-    SERVER_PROTOCOL: string;
-    SERVER_PORT: number;
-    SERVER_HOST: string;
-    SERVER_URL: string;
-    STATIC_PROTOCOL: string;
-    STATIC_PORT: number;
-    STATIC_HOST: string;
-    STATIC_URL: string;
-    KEYS_FOLDER: string;
-}
-
 /* tslint:disable object-literal-sort-keys */
-const Env: IEnv = {
-    // @ts-ignore
+const Env = {
     SERVER_PROTOCOL,
-    // @ts-ignore
     SERVER_PORT,
-    // @ts-ignore
     SERVER_HOST,
-    // @ts-ignore
     SERVER_URL,
-    // @ts-ignore
     STATIC_PROTOCOL,
-    // @ts-ignore
     STATIC_PORT,
-    // @ts-ignore
     STATIC_HOST,
-    // @ts-ignore
     STATIC_URL,
-    // @ts-ignore
     KEYS_FOLDER,
 };
 /* tslint:enable object-literal-sort-keys */
