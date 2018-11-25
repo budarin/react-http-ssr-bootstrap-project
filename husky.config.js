ceb31fb705675bb9bd09ff7c41ad839e972568a9
vadim_budarin <budarin-vv@yandex.ru>
2018-11-25 13:53:34 +0300

    [dev] git hooks

--- husky.config.js
+++ husky.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     hooks: {
-        'prepare-commit-msg': './tools/git-commit-message.sh',
+        'prepare-commit-msg': './tools/git/git-commit-message.sh',
         'pre-commit': 'pretty-quick --staged',
-        'pre-push': './tools/preventPushToMaster.sh && npm test',
+        'pre-push': './tools/git/preventPushToMaster.sh && npm test',
         'post-checkout': 'check-dependencies --install',
         'post-merge': 'check-dependencies --install',
     },
