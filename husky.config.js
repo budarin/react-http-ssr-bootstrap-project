a766df96d78137af2ddf1c98900c980d94cde051
vadim_budarin <budarin-vv@yandex.ru>
2018-11-25 13:47:32 +0300

    [dev] prepush hook

--- husky.config.js
+++ husky.config.js
@@ -2,7 +2,7 @@ module.exports = {
     hooks: {
         'prepare-commit-msg': './tools/git-commit-message.sh',
         'pre-commit': 'pretty-quick --staged',
-        'pre-push': 'npm test',
+        'pre-push': './tools/preventPushToMaster.sh && npm test',
         'post-checkout': 'check-dependencies --install',
         'post-merge': 'check-dependencies --install',
     },
